0e776ef18f9419779d9a26a02285af4e9a6ff93b
Krzysztof <krzysiektom@op.pl>
2019-03-30 16:15:40 +0100

    zadanie 6 i 7

--- src/main/java/pl/coderslab/book/BookController.java
+++ src/main/java/pl/coderslab/book/BookController.java
@@ -1,13 +1,40 @@
 package pl.coderslab.book;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/books")
-    class BookController {
-        @RequestMapping("/hello")
-        String hello(){
-            return "{hello: World}";
-        }
+class BookController {
+
+    MemoryBookService memoryBookService;
+
+    public BookController(MemoryBookService memoryBookService) {
+        this.memoryBookService = memoryBookService;
     }
+
+    @RequestMapping("/hello")
+    String hello() {
+        return "{hello: World}";
+    }
+
+    @RequestMapping("/helloBook")
+    Book helloBook() {
+        return new Book(1L, "9788324631766", "Thinking in Java",
+                "Bruce Eckel", "Helion", "programming");
+    }
+
+    @GetMapping("")
+    List<Book> books() {
+        return memoryBookService.getList();
+    }
+
+    @GetMapping("/{id}")
+    Book bookById(@PathVariable long id) {
+        return memoryBookService.getBookById(id);
+    }
+}
---
+++ src/main/java/pl/coderslab/book/MemoryBookService.java
@@ -0,0 +1,32 @@
+package pl.coderslab.book;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+class MemoryBookService {
+    private List<Book> list = new ArrayList<>();
+
+    MemoryBookService() {
+        list.add(new Book(1L, "9788324631766", "Thinking in Java", "Bruce Eckel",
+                "Helion", "programming"));
+        list.add(new Book(2L, "9788324627738", "Rusz glowa, Java.",
+                "Sierra Kathy, Bates Bert", "Helion", "programming"));
+        list.add(new Book(3L, "9780130819338", "Java 2. Podstawy",
+                "Cay Horstmann, Gary Cornell", "Helion", "programming"));
+    }
+
+    List<Book> getList() {
+        return list;
+    }
+
+    Book getBookById(long id) {
+        return list.stream().filter(b -> b.getId() == id).findAny().orElse(new Book());
+    }
+
+    public void setList(List<Book> list) {
+        this.list = list;
+    }
+}
